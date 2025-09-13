5a6d394bbf5245fdf11439eefe99af1da29e5856
jayanth909-wq <jackiejayanth909@gmail.com>
2025-09-13 11:41:32 +0530

    Test Class four

---
+++ src/_Java_Examples.java
@@ -0,0 +1,13 @@
+public class _Java_Examples
+{
+    public static void main(String[] args) {
+        int good = 54;
+        byte sweet = 12;
+        long  night = 987;
+        boolean king = false;
+        System.out.println(good);
+        System.out.println(sweet);
+        System.out.println(night);
+        System.out.println(king);
+    }
+}
